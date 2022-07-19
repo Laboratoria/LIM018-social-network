5293362f31e81d92e67222212594aa94c38b57a9
Judy Palacios <palacios.carolina0@gmail.com>
2022-07-18 20:46:11 -0500

    css y forms actualizados

--- src/views/log-in-view.js
+++ src/views/log-in-view.js
@@ -6,20 +6,21 @@ export const createLoginView = () =>
   <div class="containerForm">
     <div class="view">
       <h1 class="titles"> THE SOCIAL FOOD </h1>
-      <form class="view">
-        <input type="email" placeholder="Email" id="userEmail" class="emailPassword" value="">
+      <form>
+        <input type="email" placeholder="Email" id="userEmail" class="emailPassword" value="" class='emailPassword'>
         <br>
-        <input type="password" placeholder="Contraseña" id="password" class="emailPassword" value="">
+        <input type="password" placeholder="Contraseña" id="password" class="emailPassword" value="" class='emailPassword'>
         <br>
         <button type="button" id="submitLogIn" class="buttonsForm" >Log In</button>
       </form>
         <p>o</p>
-        <button type="button" id="gmailLogIn">Log In with gmail</button>
+        <button type="button" id="gmailLogIn" class="gmail"><img src="./images/google-mas.png" class='googleImage'>  Log In with gmail</button>
         <h3>Si no tienes cuenta, crea una <span><a href="#/sign-up">aquí</a></span></h3>
     </div>
   </div>  `;
 
   const divElement = document.createElement('div');
+  divElement.setAttribute('class', 'backgroundImage');
   divElement.innerHTML = viewLogin;
 
   return divElement;
