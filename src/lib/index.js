20812b95da5ae99dac6266748f5e03830905d2a9
belen <anabelenarroyo21@gmail.com>
2022-08-04 12:52:02 -0500

    modificado  index.js y Register.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,26 +1,40 @@
 
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
-    import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-  
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-      apiKey: "AIzaSyCrjTLw9QLhoJzC7qHJiKwFu3DRdtj1xgY",
-      authDomain: "laberintolector-5688a.firebaseapp.com",
-      projectId: "laberintolector-5688a",
-      storageBucket: "laberintolector-5688a.appspot.com",
-      messagingSenderId: "800505099476",
-      appId: "1:800505099476:web:e8781a0717d6a00bbbf741",
-      measurementId: "G-4MXS3Q2K9Q"
-    };
-  
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCrjTLw9QLhoJzC7qHJiKwFu3DRdtj1xgY",
+    authDomain: "laberintolector-5688a.firebaseapp.com",
+    projectId: "laberintolector-5688a",
+    storageBucket: "laberintolector-5688a.appspot.com",
+    messagingSenderId: "800505099476",
+    appId: "1:800505099476:web:e8781a0717d6a00bbbf741",
+    measurementId: "G-4MXS3Q2K9Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Register users
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+
 
 
--- src/view/Register.js
+++ src/view/Register.js
@@ -4,9 +4,9 @@ export default ()=> {
     <label>Nombre</label>
     <input type="text">
     <label>Email</label>
-    <input type="text">
+    <input type="text" id="email">
     <label>Contraseña</label>
-    <input type="text">
+    <input type="text id="password">
     <button class="button-register" id="register">
         Registrarse
     </button>`
@@ -16,3 +16,10 @@ export default ()=> {
 
     return divElement;
 }
+
+const inputEmail = document.querySelectorAll('#email')
+const inputPassword = document.querySelector('#password')
+const btnRegister = document.querySelectorAll('#register')
+
+btnRegister.addEventListener('click',()=>{console.log('hola')})
+
