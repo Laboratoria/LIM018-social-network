8e2b6b5398be9089a71ac3b31c66ccd51066b464
belen <anabelenarroyo21@gmail.com>
2022-08-08 06:38:39 -0500

    inicio tests de funcion acces

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,29 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+import { accesFunctions, Acces } from '../src/view/Acces.js';
+import {components} from '../src/view/index.js'
 
-describe('myFunction', () => {
+jest.mock('../src/lib/index.js', () =>{
+  return{
+    __esModule: true,
+    // ...originModule,
+    // signInWithEmailAndPassword: jest.fn(auth, user, password) => 'mocked baz',
+  }
+})
+
+describe('testeando accesFunctions', () => {
   it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+    expect(typeof accesFunctions).toBe('function');
+  });
+  it('existe el boton acces', () => {
+    const container = document.getElementById('container')
+    console.log(container)
+    //container.innerHTML = ''
+    container.appendChild(components.acces())
+    console.log(container)
+    // accesFunctions()
+    // const btnAcces = document.getElementById('acces')
+    // console.log(btnAcces)
   });
 });
+
+
