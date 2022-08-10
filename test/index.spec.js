228afb220a7cf1a516c9f85b531a661694362cfa
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-08-09 19:21:08 -0500

    test html

--- test/index.spec.js
+++ test/index.spec.js
@@ -15,15 +15,35 @@ describe('testeando default', () => {
   });
   it('existe el boton acces', () => {
     const container = document.createElement('section')
+    document.body.appendChild(container)
     container.appendChild(defaultExport())
+    const email = document.getElementById('email')
+    const password = document.getElementById('password')
+    const acces = document.getElementById('acces')
+    console.log('email')
+    expect(email instanceof HTMLElement).toBeTruthy();
+    expect(password instanceof HTMLElement).toBe(true);
+    expect(acces instanceof HTMLElement).toBeTruthy();
     console.log(container)
   });
 });
 
-describe('testeando accesFunctions', () => {
+describe('testeando accesFunctions1', () => {
   it('debería ser una función', () => {
     expect(typeof accesFunctions).toBe('function');
   });
+  it('debería ejecutar la funcion accesFunctions2', () => {
+    expect(typeof accesFunctions).toBe('function');
+  });
+
 });
 
+describe('testeando accesFunctions2', () => {
+  it('debería ser una función', () => {
+    expect(typeof accesFunctions).toBe('function');
+  });
+  it('debería ejecutar la funcion singIn', () => {
+    expect(typeof accesFunctions).toBe('function');
+  });
 
+});
