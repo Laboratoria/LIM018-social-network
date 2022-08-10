33f2da39c6245bc97187aaae8f7f6625b7568b9d
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-08-09 19:19:46 -0500

    test btn-acces

--- src/view/Acces.js
+++ src/view/Acces.js
@@ -1,7 +1,7 @@
-import {signIn} from '../lib/index.js'
+import { signIn } from '../lib/index.js'
 
-export default ()=> {
-    const viewAcces = `
+export default () => {
+  const viewAcces = `
     <h2>Bienvenido</h2>
     <label>Email</label>
     <input type="text" id="email">
@@ -11,33 +11,38 @@ export default ()=> {
         Acceder
     </button>`
 
-    const divElement = document.createElement('div')
-    divElement.innerHTML = viewAcces;
+  const divElement = document.createElement('div')
+  divElement.innerHTML = viewAcces;
 
-    return divElement;
+  return divElement;
 }
 
-export const accesFunctions = () => {
-    const btnAcces = document.getElementById('acces')
-    btnAcces.addEventListener('click', (e) => {
-        console.log('hola accediendo')
-        const user = document.getElementById('email').value
-        const password = document.getElementById('password').value
-        console.log(user, password)
-        signIn(user, password)
-          .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            window.location.hash ="#/Post"
-            // ...
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(error.code, error.message)
-            // ..
-          });
-        
-    })
+export const accesFunctions1 = () => {
+  const btnAcces = document.getElementById('acces')
+  btnAcces.addEventListener('click', (e) => {
+    console.log('hola accediendo')
+    const user = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    console.log(user, password)
 
+
+  })
+
+}
+
+
+export const accesFunctions2 = () => {
+  signIn(user, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      window.location.hash = "#/Post"
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(error.code, error.message)
+      // ..
+    });
 }
