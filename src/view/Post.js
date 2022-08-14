c95e2a642e7176ba1c4b455a76d1f62506836ebf
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-08-13 19:31:00 -0500

    style-Post

--- src/view/Post.js
+++ src/view/Post.js
@@ -1,7 +1,11 @@
 export default ()=> {
     const viewDifferent = `
+    <section class="contenido_container2" id="container2">
+    <div class = "post">
     <h2>Crea tu post</h2>
     <input type="text">
+    </div>
+    </section>>
     `
     
     const divElement = document.createElement('div')
